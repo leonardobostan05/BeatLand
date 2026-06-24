592ef5d6af4391ec75d937a21afcf1e23d96ceca
Leonardo Bostan <leonardobostan05@gmail.com>
2026-06-24 18:12:37 +0200

    Create repository, add User entity and database configuration

---
+++ src/main/java/org/leonardo/beatland/entity/User.java
@@ -0,0 +1,63 @@
+package org.leonardo.beatland.entity;
+
+import jakarta.persistence.*;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "users")
+
+
+public class User {
+
+    public User() {}
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String username;
+    private String email;
+    private String password;
+    private LocalDateTime createdAt;
+
+
+    //Getter & Setter Methoden
+
+    public Long getId(){
+        return id;
+    }
+
+    public String getUsername(){
+        return username;
+    }
+
+    public String getEmail(){
+        return email;
+    }
+
+    public String getPassword(){
+        return password;
+    }
+
+    public LocalDateTime getCreatedAt(){
+        return createdAt;
+    }
+
+    public void setUsername(String username){
+        this.username = username;
+    }
+
+    public void setEmail(String email){
+        this.email = email;
+    }
+
+    public void setPassword(String password){
+        this.password = password;
+    }
+
+    public void setCreatedAt(LocalDateTime createdAt){
+        this.createdAt = createdAt;
+    }
+
+}
---
+++ src/test/java/org/leonardo/beatland/BeatlandApplicationTests.java
@@ -0,0 +1,13 @@
+package org.leonardo.beatland;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeatlandApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
